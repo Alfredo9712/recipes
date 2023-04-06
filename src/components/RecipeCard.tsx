cb5b36211b72efb0b6e78d00bbbaff25aaeba141
Alfredo Mora <alfredomora@Alfredos-MacBook-Air.local>
2023-04-06 13:17:59 -0700

    add img to recipe schema

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,13 +1,25 @@
+import Image from "next/image";
+
 import type { FC } from "react";
 import type { RouterOutputs } from "~/utils/api";
+
+import styles from "../styles/modules/RecipeCard.module.scss";
 
 type Recipe = RouterOutputs["recipes"]["getAll"][number];
 
 export const RecipeCard: FC<Recipe> = (props) => {
-  const { name, rating, views } = props;
+  const { name, rating, views, img } = props;
 
   return (
-    <div>
+    <div className={styles.recipeCard}>
+      <div className={styles.cardImgWrapper}>
+        <Image
+          src={String(img)}
+          fill
+          alt={`recipe image for ${String(name)}`}
+          className={styles.img}
+        />
+      </div>
       <h2>{name}</h2>
       <h3>Rating - {rating}</h3>
       <h4>Views - {views}</h4>
