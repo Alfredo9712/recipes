ff2fab1b60b202b99dae7ef9eb5af74aec9d3a2a
Alfredo Mora <alfredomora@Alfredos-Air.lan1>
2023-04-07 21:56:32 -0700

    add helper util and remove margin from all elements

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 
 import type { FC } from "react";
 import type { RouterOutputs } from "~/utils/api";
+import { convertSecondsToMins } from "~/utils/clientHelpers";
 
 import styles from "../styles/modules/RecipeCard.module.scss";
 
 type Recipe = RouterOutputs["recipes"]["getAll"][number];
 
 export const RecipeCard: FC<Recipe> = (props) => {
-  const { name, img, id } = props;
+  const { name, img, id, duration } = props;
   console.log(img);
   return (
     <div
@@ -19,6 +20,9 @@ export const RecipeCard: FC<Recipe> = (props) => {
     >
       <div className={styles.cardContent}>
         <h2>{`${String(name)}`}</h2>
+        <div className={styles.recipeInfo}>
+          <p>{convertSecondsToMins(Number(duration))}</p>
+        </div>
       </div>
     </div>
   );
---
+++ src/utils/clientHelpers.ts
@@ -0,0 +1,1 @@
+export const convertSecondsToMins = (seconds: number) => seconds / 60;
