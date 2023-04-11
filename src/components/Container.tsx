39f87ef6c00bf1ca11537842906af87fd98fa73c
Alfredo Mora <alfredomora@Alfredos-MacBook-Air.local>
2023-04-10 19:52:12 -0700

    add container wrapper and create tablet and desktop widths

---
+++ src/components/Container.tsx
@@ -0,0 +1,7 @@
+import type { PropsWithChildren } from "react";
+
+import styles from "../styles/modules/Container.module.scss";
+
+export const Container = (props: PropsWithChildren) => {
+  return <div className={styles.container}>{props.children}</div>;
+};
