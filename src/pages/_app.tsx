1c47f9f6a22b03e892bcd6880c7a57dc39da5141
Alfredo Mora <alfredomora@Alfredos-MacBook-Air.local>
2023-04-14 22:54:06 -0700

    add global head and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,23 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+import { Container } from "~/components/Container";
+import Head from "next/head";
+
 import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import { Container } from "~/components/Container";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
       <Container>
+        <Head>
+          <title>TRPC Recipes</title>
+          <meta name="description" content="🥗" />
+          <link rel="icon" href="/recipes.icon.png" />
+        </Head>
         <Component {...pageProps} />
       </Container>
     </ClerkProvider>
