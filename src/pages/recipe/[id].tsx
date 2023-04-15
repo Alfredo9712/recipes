e3ee125ec045fbf1e229e018e8391d13f029f6bd
Alfredo Mora <alfredomora@Alfredos-MacBook-Air.local>
2023-04-14 22:47:14 -0700

    add server side rendering to recipe page

--- src/pages/recipe/[id].tsx
+++ src/pages/recipe/[id].tsx
@@ -1,20 +1,21 @@
-import { useRouter } from "next/router";
+import { TRPCError } from "@trpc/server";
 
+import type { GetStaticProps } from "next";
+import type { FC } from "react";
+
+import { generateSSGHelper } from "~/server/helpers/sshHelper";
 import { api } from "~/utils/api";
 
-const Recipe = () => {
-  const router = useRouter();
-  const { id = "" } = router.query as { id: string };
-
+const Recipe: FC<{ recipeId: string }> = ({ recipeId }) => {
   const ctx = api.useContext();
 
   const { data: recipe, isLoading } = api.recipes.getById.useQuery({
-    id: id,
+    id: recipeId,
   });
 
   const { mutate, isLoading: isFavoriting } = api.recipes.favorite.useMutation({
     onSuccess: () => {
-      void ctx.recipes.getById.invalidate({ id: id });
+      void ctx.recipes.getById.invalidate({ id: recipeId });
     },
   });
 
@@ -25,9 +26,31 @@ const Recipe = () => {
   return (
     <div>
       <p>favorites: {recipe.favorites} </p>
-      <button onClick={() => mutate({ id })}>Favorite</button>
+      <button onClick={() => mutate({ id: recipeId })}>Favorite</button>
     </div>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper();
+
+  const recipeId = context.params?.id;
+
+  if (typeof recipeId !== "string")
+    throw new TRPCError({ code: "BAD_REQUEST" });
+
+  await ssg.recipes.getById.prefetch({ id: recipeId });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      recipeId,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
 };
 
 export default Recipe;
